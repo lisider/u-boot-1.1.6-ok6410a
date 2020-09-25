8c5b50504853eb43129279b38c4df579c64c751c
lisider <1368317399@qq.com>
2020-09-25 12:14:53 +0800

    删除多余的板级文件

---
+++ build.sh
@@ -0,0 +1,9 @@
+#########################################################################
+# File Name: build.sh
+# Author: Sues
+# mail: sumory.kaka@foxmail.com
+# Created Time: Fri 25 Sep 2020 10:36:07 AM CST
+# Version : 1.0
+#########################################################################
+#!/bin/bash
+make  ARCH=arm CROSS_COMPILE=arm-linux- -j20
---
+++ config.sh
@@ -0,0 +1,9 @@
+#########################################################################
+# File Name: config.sh
+# Author: Sues
+# mail: sumory.kaka@foxmail.com
+# Created Time: Fri 25 Sep 2020 10:36:02 AM CST
+# Version : 1.0
+#########################################################################
+#!/bin/bash
+make  ARCH=arm CROSS_COMPILE=arm-linux- smdk6410_config
